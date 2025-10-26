a8e43255fae4d58a11ae78a53635e7317638115d
alpha9av <alpha9coder@gmail.com>
2025-10-27 01:20:35 +0530

    remove old entry point file

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { Wireframe } from "./screens/Wireframe/Wireframe";
-
-createRoot(document.getElementById("app") as HTMLElement).render(
-  <StrictMode>
-    <Wireframe />
-  </StrictMode>,
-);
